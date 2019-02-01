5098000426754a8ea8ab03fa3fc87f322e3877ee
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2019-02-01 12:24:33 +0000

    Suppresion des logs de test

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
     let taille = screen.availHeight;
     $('header').css('height',taille);
-
-    console.log($('htnl').css('height'));
+    
     let imgT = $('#gastronomie img').css('height');
      imgT = parseFloat(imgT) + 40 + 'px';
-
-    console.log($('#app').css('width'));
-
-    console.log($('#header').css('width'));
-
     $('#gastronomie .case').css('height',imgT);
 });
